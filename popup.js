c7568679f84ae1339bd974e8f3e32c944f6bfa25
Thomas LAPRIE <thomas.laprie@epitech.eu>
2023-05-11 16:16:51 +0200

    Update: big part

--- popup.js
+++ popup.js
@@ -1,23 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('injectButton').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var currentTab = tabs[0];
-        chrome.scripting.executeScript({
-          target: { tabId: currentTab.id },
-          function: injectDiv
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            var currentTab = tabs[0];
+            var url = currentTab.url;
+            var regex = /^https:\/\/www\.google\.com\/maps/;
+            if (regex.test(url)) {
+                chrome.scripting.executeScript({
+                target: { tabId: currentTab.id },
+                function: injectDiv
+                });
+            }
         });
-      });
     });
   });
 
   function injectDiv() {
-    var div = document.createElement('div');
-    div.innerHTML = 'Contenu de la div';
-    var div2 = document.querySelector("#QA0Szd > div > div > div.w6VYqd > div.bJzME.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb");
-    if (div2 != null)
-        div2.append(div);
-    else
-        console.log("lalalalala")
-    console.log(div2)
-    console.log(document.querySelector("#omnibox-directions > div > div:nth-child(2) > div > div > div > div:nth-child(2)"))
+    var div = document.querySelector("#QA0Szd > div > div > div.w6VYqd > div.bJzME.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb");
+    console.log(div);
+    var divEnfants = div.getElementsByClassName("MespJc");
+    // // #section-directions-trip-1 > div.MespJc
+    for (var i = 0; i < divEnfants.length; i++) {
+        var rand = Math.floor(Math.random() * 2000) + 100;
+        var divEnfant = divEnfants[i];
+        for (var j = 0; j < divEnfant.children.length; j++) {
+            var divEnfantEnfant = divEnfant.children[j];
+            console.log(divEnfantEnfant);
+            var p1 = document.createElement('p');
+            p1.innerHTML = 'Carbon footprint : ' + rand + 'g CO2';
+            if (rand < 1000)
+                p1.style.color = 'green';
+            else if (rand < 1500)
+                p1.style.color = 'orange';
+            else
+                p1.style.color = 'red';
+            divEnfantEnfant.append(p1);
+        }
+    //     var p1 = document.createElement('p');
+    //     p1.innerHTML = 'Carbon footprint : ' + rand + 'g CO2';
+    //     console.log(p1);
+    //     var p2 = document.createElement('p');
+    //     p2.innerHTML = 'Carbon footprint : ' + rand + 'g CO2';
+    //     console.log(divEnfant);
+    // //     console.log(divEnfant.querySelector('div[jstcache="157"]'));
+    // //     console.log(divEnfant.querySelector('div[jstcache="158"]'));
+    //     divEnfant.querySelector('div[jstcache="157"]').append(p1);
+    //     divEnfant.querySelector('div[jstcache="158"]').append(p2);
+    //     console.log('-----------------')
+    }
+
   }
