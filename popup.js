6e6f412f3ed150a1cb2d4e547d8e58a1151f7f08
Thomas LAPRIE <thomas.laprie@epitech.eu>
2023-05-11 18:47:49 +0200

    Update: no html

--- popup.js
+++ popup.js
@@ -1,24 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('injectButton').addEventListener('click', function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+chrome.action.onClicked.addListener(function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             var currentTab = tabs[0];
             var url = currentTab.url;
             var regex = /^https:\/\/www\.google\.com\/maps/;
             if (regex.test(url)) {
                 chrome.scripting.executeScript({
-                target: { tabId: currentTab.id },
-                function: injectDiv
+                    target: { tabId: currentTab.id },
+                    function: injectDiv
                 });
             }
         });
-    });
-  });
-
+});
   function injectDiv() {
     var div = document.querySelector("#QA0Szd > div > div > div.w6VYqd > div.bJzME.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.m6QErb");
-    // console.log(div);
     var divEnfants = div.getElementsByClassName("MespJc");
-    // // #section-directions-trip-1 > div.MespJc
     for (var i = 0; i < divEnfants.length; i++) {
         var rand = Math.floor(Math.random() * 2000) + 100;
         var divEnfant = divEnfants[i];
@@ -52,17 +47,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 divEnfantEnfant.append(p1);
             }
         }
-    //     var p1 = document.createElement('p');
-    //     p1.innerHTML = 'Carbon footprint : ' + rand + 'g CO2';
-    //     console.log(p1);
-    //     var p2 = document.createElement('p');
-    //     p2.innerHTML = 'Carbon footprint : ' + rand + 'g CO2';
-    //     console.log(divEnfant);
-    // //     console.log(divEnfant.querySelector('div[jstcache="157"]'));
-    // //     console.log(divEnfant.querySelector('div[jstcache="158"]'));
-    //     divEnfant.querySelector('div[jstcache="157"]').append(p1);
-    //     divEnfant.querySelector('div[jstcache="158"]').append(p2);
-    //     console.log('-----------------')
     }
 
     function getTime(div) {
